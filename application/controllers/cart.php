ebc7f5632e06c7d7db52c2b1603fbb157b463fd9
hereiam123 <bdemaio1@gmail.com>
2017-06-02 16:26:34 -0400

    added ability to add items to a cart, but does not reflect on page yet. The cart library was deprecated in codeigniter 2, but since we are using codeigniter 3, it has become much safer due to a refactoring/reimplementation of session. THis cart will not be real, it is for proof of concept in this application

--- application/controllers/cart.php
+++ application/controllers/cart.php
@@ -3,10 +3,10 @@
 	class Cart extends CI_Controller{
 
 		public $paypal_data = '';
-		public tax;
-		public shipping;
-		public total = 0;
-		public grand_total;
+		public $tax;
+		public $shipping;
+		public $total = 0;
+		public $grand_total;
 
 		/*
 		*	Cart index
@@ -16,7 +16,27 @@
 			$data['main_content']='cart';
 			$this->load->view('layouts/main',$data);
 		}
-		
+
+		/*
+		*	Add to cart
+		*/
+		public function add(){
+			//item data
+			$data = array(
+				'id' => $this->input->post('item_number'),
+				'qty' => $this->input->post('qty'),
+				'price' => $this->input->post('price'),
+				'name' => $this->input->post('title')
+			);
+
+			//print_r($data); die();
+
+			//Insert into cart
+			$this->cart->insert($data);
+
+			redirect('products');
+		}
+
 	}
 
 ?>
