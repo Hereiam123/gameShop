9dbab80781f4a37b09cda9ec62ecbaa387e01cc4
hereiam123 <bdemaio1@gmail.com>
2017-06-02 13:42:40 -0400

    game(products) now displayed dynamically from the database

--- application/views/products.php
+++ application/views/products.php
@@ -1,3 +1,14 @@
 <?php foreach($products as $product) :?>
-	<?php echo $product->title;?><br>
+	<div class="col-md-4 game">
+		<div class="game-price"><?php echo $product->price;?></div>
+		<a href="<?php base_url(); ?>products/details/<?php echo $product->id; ?>">
+			<img src="<?php echo base_url();?>assets/img/<?php echo $product->image; ?>" />
+		</a>
+		<div class="game-title">
+			<?php echo $product->title; ?>
+		</div>
+		<div class="game-add">
+			<button class="btn btn-primary" type="submit">Add To Cart</button>
+		</div>
+	</div>
 <?php endforeach; ?>
