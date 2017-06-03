e5ad4bacabf2b94db707e8d3731760822a38f5e2
hereiam123 <bdemaio1@gmail.com>
2017-06-03 19:12:46 -0400

    prepping validation rules in user controller

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -3,9 +3,22 @@
 	class Users extends CI_Controller{
 
 		public function register(){
-			//show register view
-			$data['main_content'] = 'register';
-			$this->load->view('layouts/main',$data);
+
+			//Validation rules
+			$this->form_validation->set_rules('first_name','First name','trim|required|min_lenght[5]|max_length[20]');
+			$this->form_validation->set_rules('last_name','Last name','required');
+			$this->form_validation->set_rules('','First name','required');
+			$this->form_validation->set_rules('first_name','First name','required');
+
+			if ($this->form_validation->run() == FALSE){
+					//show register view
+					$data['main_content'] = 'register';
+					$this->load->view('layouts/main',$data);
+			}
+				else{
+					$this->load->view('formsuccess');
+			}
+
 		}
 
 	}
