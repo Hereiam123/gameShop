798a3b50a6398c4bddd2721adcff9c8e0ac809a5
hereiam123 <bdemaio1@gmail.com>
2017-06-16 20:38:01 -0400

    heroku push

--- application/config/config.php
+++ application/config/config.php
@@ -24,11 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
-$root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-$config['base_url'] = $root;
-
-/*$config['base_url'] = 'https://fierce-coast-30613.herokuapp.com/';*/
+$config['base_url'] = 'https://fierce-coast-30613.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
@@ -40,7 +36,7 @@ $config['base_url'] = $root;
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
