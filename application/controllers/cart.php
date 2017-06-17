8eb8c90fd65655225276908a0834832169daa153
hereiam123 <bdemaio1@gmail.com>
2017-06-16 20:40:30 -0400

    heroku push

--- application/controllers/cart.php
+++ application/controllers/cart.php
@@ -1,201 +1,201 @@
 <?php
 
-	class Cart extends CI_Controller{
+class Cart extends CI_Controller{
 
-		public $paypal_data = '';
-		public $tax;
-		public $shipping;
-		public $total = 0;
-		public $grand_total;
+	public $paypal_data = '';
+	public $tax;
+	public $shipping;
+	public $total = 0;
+	public $grand_total;
 
-		/*
-		*	Cart index
-		*/
-		public function index(){
-			//Get cart view
-			$data['main_content']='cart';
-			$this->load->view('layouts/main',$data);
-		}
+	/*
+	*	Cart index
+	*/
+	public function index(){
+		//Get cart view
+		$data['main_content']='cart';
+		$this->load->view('layouts/main',$data);
+	}
 
-		/*
-		*	Add to cart
-		*/
-		public function add(){
-			//item data
-			$data = array(
-				'id' => $this->input->post('item_number'),
-				'qty' => $this->input->post('qty'),
-				'price' => $this->input->post('price'),
-				'name' => $this->input->post('title')
-			);
+	/*
+	*	Add to cart
+	*/
+	public function add(){
+		//item data
+		$data = array(
+			'id' => $this->input->post('item_number'),
+			'qty' => $this->input->post('qty'),
+			'price' => $this->input->post('price'),
+			'name' => $this->input->post('title')
+		);
 
-			//Insert into cart
-			$this->cart->insert($data);
+		//Insert into cart
+		$this->cart->insert($data);
 
-			redirect('products');
-		}
+		redirect('products');
+	}
 
-		/*
-		*	Update Cart
-		*/
-		public function update($in_cart=null){
-			$data = $_POST;
-			$this->cart->update($data);
+	/*
+	*	Update Cart
+	*/
+	public function update($in_cart=null){
+		$data = $_POST;
+		$this->cart->update($data);
 
-			//Show cart page
-			redirect('products','refresh');
-		}
+		//Show cart page
+		redirect('products','refresh');
+	}
 
-		/*
-		*	Process Cart Order
-		*/
-		public function process(){
-			if($_POST){
-				foreach($this->input->post('item_name') as $key=>$value){
-					//Get tax and shipping from config file
-					$this->tax=$this->config->item('tax');
-					$this->shipping=$this->config->item('shipping');
+	/*
+	*	Process Cart Order
+	*/
+	public function process(){
+		if($_POST){
+			foreach($this->input->post('item_name') as $key=>$value){
+				//Get tax and shipping from config file
+				$this->tax=$this->config->item('tax');
+				$this->shipping=$this->config->item('shipping');
 
-					$item_id=$this->input->post('item_code')[$key];
-					$product=$this->Product_model->get_product_details($item_id);
+				$item_id=$this->input->post('item_code')[$key];
+				$product=$this->Product_model->get_product_details($item_id);
 
-					//Assign data to Paypal
-					$this->paypal_data .= '&L_PAYMENTREQUEST_0_NAME'.$key.'='.urlencode($product->title);
-					$this->paypal_data .= '&L_PAYMENTREQUEST_0_NUMBER'.$key.'='.urlencode($item_id);
-					$this->paypal_data .= '&L_PAYMENTREQUEST_0_AMT'.$key.'='.urlencode($product->price);
-					$this->paypal_data .= '&L_PAYMENTREQUEST_0_QTY'.$key.'='. urlencode($this->input->post('item_qty')[$key]);
+				//Assign data to Paypal
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_NAME'.$key.'='.urlencode($product->title);
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_NUMBER'.$key.'='.urlencode($item_id);
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_AMT'.$key.'='.urlencode($product->price);
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_QTY'.$key.'='. urlencode($this->input->post('item_qty')[$key]);
 
-					//Price x Quantity
-					$subtotal = ($product->price * $this->input->post('item_qty')[$key]);
-					$this->total = $this->total + $subtotal;
+				//Price x Quantity
+				$subtotal = ($product->price * $this->input->post('item_qty')[$key]);
+				$this->total = $this->total + $subtotal;
 
-					$paypal_product['items'][]=array(
-						'itm_name'=>$product->title,
-						'itm_price'=>$product->price,
-						'itm_code'=>$item_id,
-						'itm_qty'=>$this->input->post('item_qty')[$key]
-					);
-
-					//Array for order to be inserted
-					$order_data = array(
-						'product_id'=>$item_id,
-						'user_id'=>$this->session->userdata('user_id'),
-						'transaction_id'=>0,
-						'qty'=>$this->input->post('item_qty')[$key],
-						'price'=>$subtotal,
-						'address'=>$this->input->post('address'),
-						'address2'=>$this->input->post('address2'),
-						'city'=>$this->input->post('city'),
-						'state'=>$this->input->post('state'),
-						'zipcode'=>$this->input->post('zipcode')
-					);
-
-					//Add to database, order table
-					$this->Product_model->add_order($order_data);
-				}
-
-				//Get grand total
-				$this->grand_total = $this->total + $this->tax + $this->shipping;
-
-				//Cost array
-				$paypal_product['assets']=array(
-					'tax_total'=>$this->tax,
-					'shipping_cost'=>$this->shipping,
-					'grand_total'=>$this->total
+				$paypal_product['items'][]=array(
+					'itm_name'=>$product->title,
+					'itm_price'=>$product->price,
+					'itm_code'=>$item_id,
+					'itm_qty'=>$this->input->post('item_qty')[$key]
 				);
 
-				//Session array for when returned
-				$_SESSION['paypal_products']=$paypal_product;
+				//Array for order to be inserted
+				$order_data = array(
+					'product_id'=>$item_id,
+					'user_id'=>$this->session->userdata('user_id'),
+					'transaction_id'=>0,
+					'qty'=>$this->input->post('item_qty')[$key],
+					'price'=>$subtotal,
+					'address'=>$this->input->post('address'),
+					'address2'=>$this->input->post('address2'),
+					'city'=>$this->input->post('city'),
+					'state'=>$this->input->post('state'),
+					'zipcode'=>$this->input->post('zipcode')
+				);
 
-				//Send Paypal Params
-				$padata = 	'&METHOD=SetExpressCheckout'.
-						'&RETURNURL='.urlencode($this->config->item('paypal_return_url')).
-						'&CANCELURL='.urlencode($this->config->item('paypal_cancel_url')).
-						'&PAYMENTREQUEST_0_PAYMENTACTION='.urlencode("SALE").
-						$this->paypal_data.
-						'&NOSHIPPING=0'.
-						'&PAYMENTREQUEST_0_ITEMAMT='.urlencode($this->total).
-						'&PAYMENTREQUEST_0_TAXAMT='.urlencode($this->tax).
-						'&PAYMENTREQUEST_0_SHIPPINGAMT='.urlencode($this->shipping).
-						'&PAYMENTREQUEST_0_AMT='.urlencode($this->grand_total).
-						'&PAYMENTREQUEST_0_CURRENCYCODE='.urlencode($this->config->item('paypal_currency_code')).
-						'&LOCALECODE=GB'. //PayPal pages to match the language on your website.
-						'&CARTBORDERCOLOR=FFFFFF'.
-						'&ALLOWNOTE=1';
-
-				//Execute "SetExpressCheckOut"
-				$httpParsedResponseAr = $this->paypal->PPHttpPost('SetExpressCheckout', $padata, $this->config->item('paypal_api_username'), $this->config->item('paypal_api_password'), $this->config->item('paypal_api_signature'), $this->config->item('paypal_mode'));
-
-				//Respond according to message we receive from Paypal
-				if("SUCCESS" == strtoupper($httpParsedResponseAr["ACK"]) || "SUCCESSWITHWARNING" == strtoupper($httpParsedResponseAr["ACK"])){
-					//Redirect user to PayPal store with Token received.
-					$paypal_url ='https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token='.$httpParsedResponseAr["TOKEN"].'';
-					header('Location: '.$paypal_url);
-				} else{
-					//Show error message
-					print_r($httpParsedResponseAr);
-					die(urldecode($httpParsedResponseAr["L_LONGMESSAGE0"]));
-				}
+				//Add to database, order table
+				$this->Product_model->add_order($order_data);
 			}
 
-			//Paypal redirects back to this page using ReturnURL, We should receive TOKEN and Payer ID
-			if(!empty($this->input->get('token')) && !empty($this->input->get('PayerID'))){
-				//we will be using these two variables to execute the "DoExpressCheckoutPayment"
-				//Note: we haven't received any payment yet.
+			//Get grand total
+			$this->grand_total = $this->total + $this->tax + $this->shipping;
 
-				$token = $this->input->get('token');
-				$payer_id = $this->input->get('PayerID');
+			//Cost array
+			$paypal_product['assets']=array(
+				'tax_total'=>$this->tax,
+				'shipping_cost'=>$this->shipping,
+				'grand_total'=>$this->total
+			);
 
-				//Get Session info
-				$paypal_product = $_SESSION["paypal_products"];
-				$this->paypal_data = '';
-				$total_price = 0;
+			//Session array for when returned
+			$_SESSION['paypal_products']=$paypal_product;
 
-				//Loop Through Session Array
-				foreach($paypal_product['items'] as $key => $item){
-					$this->paypal_data .= '&L_PAYMENTREQUEST_0_QTY'.$key.'='. urlencode($item['itm_qty']);
-					$this->paypal_data .= '&L_PAYMENTREQUEST_0_AMT'.$key.'='.urlencode($item['itm_price']);
-					$this->paypal_data .= '&L_PAYMENTREQUEST_0_NAME'.$key.'='.urlencode($item['itm_name']);
-					$this->paypal_data .= '&L_PAYMENTREQUEST_0_NUMBER'.$key.'='.urlencode($item['itm_code']);
+			//Send Paypal Params
+			$padata = 	'&METHOD=SetExpressCheckout'.
+					'&RETURNURL='.urlencode($this->config->item('paypal_return_url')).
+					'&CANCELURL='.urlencode($this->config->item('paypal_cancel_url')).
+					'&PAYMENTREQUEST_0_PAYMENTACTION='.urlencode("SALE").
+					$this->paypal_data.
+					'&NOSHIPPING=0'.
+					'&PAYMENTREQUEST_0_ITEMAMT='.urlencode($this->total).
+					'&PAYMENTREQUEST_0_TAXAMT='.urlencode($this->tax).
+					'&PAYMENTREQUEST_0_SHIPPINGAMT='.urlencode($this->shipping).
+					'&PAYMENTREQUEST_0_AMT='.urlencode($this->grand_total).
+					'&PAYMENTREQUEST_0_CURRENCYCODE='.urlencode($this->config->item('paypal_currency_code')).
+					'&LOCALECODE=GB'. //PayPal pages to match the language on your website.
+					'&CARTBORDERCOLOR=FFFFFF'.
+					'&ALLOWNOTE=1';
 
-					//Get Subtotal
-					$subtotal = ($item['itm_price']*$item['itm_qty']);
+			//Execute "SetExpressCheckOut"
+			$httpParsedResponseAr = $this->paypal->PPHttpPost('SetExpressCheckout', $padata, $this->config->item('paypal_api_username'), $this->config->item('paypal_api_password'), $this->config->item('paypal_api_signature'), $this->config->item('paypal_mode'));
 
-					//Get Total
-					$total_price = ($total_price + $subtotal);
-				}
+			//Respond according to message we receive from Paypal
+			if("SUCCESS" == strtoupper($httpParsedResponseAr["ACK"]) || "SUCCESSWITHWARNING" == strtoupper($httpParsedResponseAr["ACK"])){
+				//Redirect user to PayPal store with Token received.
+				$paypal_url ='https://www.sandbox.paypal.com/cgi-bin/webscr?cmd=_express-checkout&token='.$httpParsedResponseAr["TOKEN"].'';
+				header('Location: '.$paypal_url);
+			} else{
+				//Show error message
+				print_r($httpParsedResponseAr);
+				die(urldecode($httpParsedResponseAr["L_LONGMESSAGE0"]));
+			}
+		}
 
-				$padata = 	'&TOKEN='.urlencode($token).
-				'&PAYERID='.urlencode($payer_id).
-				'&PAYMENTREQUEST_0_PAYMENTACTION='.urlencode("SALE").
-				$this->paypal_data.
-				'&PAYMENTREQUEST_0_ITEMAMT='.urlencode($total_price).
-				'&PAYMENTREQUEST_0_TAXAMT='.urlencode($paypal_product['assets']['tax_total']).
-				'&PAYMENTREQUEST_0_SHIPPINGAMT='.urlencode($paypal_product['assets']['shipping_cost']).
-				'&PAYMENTREQUEST_0_AMT='.urlencode($paypal_product['assets']['grand_total']).
-				'&PAYMENTREQUEST_0_CURRENCYCODE='.urlencode($PayPalCurrencyCode);
+		//Paypal redirects back to this page using ReturnURL, We should receive TOKEN and Payer ID
+		if(!empty($this->input->get('token')) && !empty($this->input->get('PayerID'))){
+			//we will be using these two variables to execute the "DoExpressCheckoutPayment"
+			//Note: we haven't received any payment yet.
 
-				//Execute "DoExpressCheckoutPayment"
-				$httpParsedResponseAr = $this->paypal->PPHttpPost('DoExpressCheckoutPayment', $padata, $this->config->item('paypal_api_username'), $this->config->item('paypal_api_password'), $this->config->item('paypal_api_signature'), $this->config->item('paypal_mode'));
+			$token = $this->input->get('token');
+			$payer_id = $this->input->get('PayerID');
 
-				//Check if everything went ok..
-				if("SUCCESS" == strtoupper($httpParsedResponseAr["ACK"]) || "SUCCESSWITHWARNING" == strtoupper($httpParsedResponseAr["ACK"])){
-					$data['trans_id'] = urldecode($httpParsedResponseAr["PAYMENTINFO_0_TRANSACTIONID"]);
+			//Get Session info
+			$paypal_product = $_SESSION["paypal_products"];
+			$this->paypal_data = '';
+			$total_price = 0;
 
-					//Load View
-					$data['main_content'] = 'thankyou';
-					$this->load->view('layouts/main', $data);
+			//Loop Through Session Array
+			foreach($paypal_product['items'] as $key => $item){
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_QTY'.$key.'='. urlencode($item['itm_qty']);
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_AMT'.$key.'='.urlencode($item['itm_price']);
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_NAME'.$key.'='.urlencode($item['itm_name']);
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_NUMBER'.$key.'='.urlencode($item['itm_code']);
 
-					$padata = 	'&TOKEN='.urlencode($token);
-					$httpParsedResponseAr = $this->paypal->PPHttpPost('GetExpressCheckoutDetails', $padata, $this->config->item('paypal_api_username'), $this->config->item('paypal_api_password'), $this->config->item('paypal_api_signature'), $this->config->item('paypal_mode'));
-				} else {
-					die($httpParsedResponseAr["L_LONGMESSAGE0"]);
-					echo '<pre>';
-					print_r($httpParsedResponseAr);
-					echo '</pre>';
-				}
+				//Get Subtotal
+				$subtotal = ($item['itm_price']*$item['itm_qty']);
+
+				//Get Total
+				$total_price = ($total_price + $subtotal);
+			}
+
+			$padata = 	'&TOKEN='.urlencode($token).
+			'&PAYERID='.urlencode($payer_id).
+			'&PAYMENTREQUEST_0_PAYMENTACTION='.urlencode("SALE").
+			$this->paypal_data.
+			'&PAYMENTREQUEST_0_ITEMAMT='.urlencode($total_price).
+			'&PAYMENTREQUEST_0_TAXAMT='.urlencode($paypal_product['assets']['tax_total']).
+			'&PAYMENTREQUEST_0_SHIPPINGAMT='.urlencode($paypal_product['assets']['shipping_cost']).
+			'&PAYMENTREQUEST_0_AMT='.urlencode($paypal_product['assets']['grand_total']).
+			'&PAYMENTREQUEST_0_CURRENCYCODE='.urlencode($PayPalCurrencyCode);
+
+			//Execute "DoExpressCheckoutPayment"
+			$httpParsedResponseAr = $this->paypal->PPHttpPost('DoExpressCheckoutPayment', $padata, $this->config->item('paypal_api_username'), $this->config->item('paypal_api_password'), $this->config->item('paypal_api_signature'), $this->config->item('paypal_mode'));
+
+			//Check if everything went ok..
+			if("SUCCESS" == strtoupper($httpParsedResponseAr["ACK"]) || "SUCCESSWITHWARNING" == strtoupper($httpParsedResponseAr["ACK"])){
+				$data['trans_id'] = urldecode($httpParsedResponseAr["PAYMENTINFO_0_TRANSACTIONID"]);
+
+				//Load View
+				$data['main_content'] = 'thankyou';
+				$this->load->view('layouts/main', $data);
+
+				$padata = 	'&TOKEN='.urlencode($token);
+				$httpParsedResponseAr = $this->paypal->PPHttpPost('GetExpressCheckoutDetails', $padata, $this->config->item('paypal_api_username'), $this->config->item('paypal_api_password'), $this->config->item('paypal_api_signature'), $this->config->item('paypal_mode'));
+			} else {
+				die($httpParsedResponseAr["L_LONGMESSAGE0"]);
+				echo '<pre>';
+				print_r($httpParsedResponseAr);
+				echo '</pre>';
 			}
 		}
 	}
+}
 
 ?>
--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,81 +1,81 @@
 <?php
 
-	class Users extends CI_Controller{
+class Users extends CI_Controller{
 
-		public function register(){
+	public function register(){
 
-			//Validation rules
-			$this->form_validation->set_rules('first_name','First name','trim|required');
-			$this->form_validation->set_rules('last_name','Last name','trim|required');
-			$this->form_validation->set_rules('email','Email','trim|required|valid_email');
-			$this->form_validation->set_rules('username','Username','trim|required|min_length[5]|max_length[20]');
-			$this->form_validation->set_rules('password','Password','trim|required|min_length[5]|max_length[10]');
-			$this->form_validation->set_rules('password2','Password Confirmation','trim|required|matches[password]');
+		//Validation rules
+		$this->form_validation->set_rules('first_name','First name','trim|required');
+		$this->form_validation->set_rules('last_name','Last name','trim|required');
+		$this->form_validation->set_rules('email','Email','trim|required|valid_email');
+		$this->form_validation->set_rules('username','Username','trim|required|min_length[5]|max_length[20]');
+		$this->form_validation->set_rules('password','Password','trim|required|min_length[5]|max_length[10]');
+		$this->form_validation->set_rules('password2','Password Confirmation','trim|required|matches[password]');
 
-			//If form validation isn't true
-			if ($this->form_validation->run() == FALSE){
-				//show register view
-				$data['main_content'] = 'register';
-				$this->load->view('layouts/main',$data);
-			}
-			//If form is valid, test user model to see if it registers
-			else{
-				$data=array(
-					'first_name'=>$this->input->post('first_name'),
-					'last_name'=>$this->input->post('last_name'),
-					'email'=>$this->input->post('email'),
-					'username'=>$this->input->post('username'),
-					'password'=>$this->input->post('password')
-				);
-
-				if($this->User_model->register($data)){
-					//message to be returned upon successful register
-					$this->session->set_flashdata('registered', 'You are now registered');
-					redirect('products');
-				}
-			}
+		//If form validation isn't true
+		if ($this->form_validation->run() == FALSE){
+			//show register view
+			$data['main_content'] = 'register';
+			$this->load->view('layouts/main',$data);
 		}
+		//If form is valid, test user model to see if it registers
+		else{
+			$data=array(
+				'first_name'=>$this->input->post('first_name'),
+				'last_name'=>$this->input->post('last_name'),
+				'email'=>$this->input->post('email'),
+				'username'=>$this->input->post('username'),
+				'password'=>$this->input->post('password')
+			);
 
-		public function login(){
-
-			$this->form_validation->set_rules('username','Username','trim|required');
-			$this->form_validation->set_rules('password','Password','trim|required');
-
-			$username=$this->input->post('username');
-			$password=$this->input->post('password');
-
-			$user_id = $this->User_model->login($username , $password);
-
-			//See if user exists and has correct password
-			if($user_id){
-				$data = array(
-					'user_id'=>$user_id->id,
-					'username'=>$username,
-					'logged_in'=>true
-				);
-
-				//set session data for logged in user
-				$this->session->set_userdata($data);
-
-				$this->session->set_flashdata('login success','You have logged in');
-				redirect('products');
-			}
-			else{
-				//Set error message
-				$this->session->set_flashdata('failed login','Login failed');
+			if($this->User_model->register($data)){
+				//message to be returned upon successful register
+				$this->session->set_flashdata('registered', 'You are now registered');
 				redirect('products');
 			}
 		}
+	}
 
-		public function logout(){
-			//Unset user data
-			$this->session->unset_userdata('logged_in');
-			$this->session->unset_userdata('user_id');
-			$this->session->unset_userdata('username');
-			$this->session->sess_destroy();
+	public function login(){
 
+		$this->form_validation->set_rules('username','Username','trim|required');
+		$this->form_validation->set_rules('password','Password','trim|required');
+
+		$username=$this->input->post('username');
+		$password=$this->input->post('password');
+
+		$user_id = $this->User_model->login($username , $password);
+
+		//See if user exists and has correct password
+		if($user_id){
+			$data = array(
+				'user_id'=>$user_id->id,
+				'username'=>$username,
+				'logged_in'=>true
+			);
+
+			//set session data for logged in user
+			$this->session->set_userdata($data);
+
+			$this->session->set_flashdata('login success','You have logged in');
+			redirect('products');
+		}
+		else{
+			//Set error message
+			$this->session->set_flashdata('failed login','Login failed');
 			redirect('products');
 		}
 	}
+
+	public function logout(){
+		//Unset user data
+		$this->session->unset_userdata('logged_in');
+		$this->session->unset_userdata('user_id');
+		$this->session->unset_userdata('username');
+		$this->session->sess_destroy();
+
+		redirect('products');
+	}
+}
 
 ?>
